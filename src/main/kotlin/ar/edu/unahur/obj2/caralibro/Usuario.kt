26c4466f20bf93fc462c828f68f7878c7111aedf
Luch <70541571+lucianoaraus@users.noreply.github.com>
2021-04-18 22:05:30 -0300

    Update: July Lucho Usuario.

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Usuario.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Usuario.kt
@@ -8,9 +8,30 @@ class Usuario {
     publicaciones.add(publicacion)
   }
   fun espacioDePublicaciones() = publicaciones.sumBy { it.espacioQueOcupa() }
-  fun likear(publicacion: Publicacion){ publicacion.sumarLike() }
-  //No es posible que una misma persona le de me gusta más de una vez.
+  fun likear(publicacion: Publicacion) {
+    if (!publicacion.quienLikeo().contains(this)){
+      publicacion.sumarLike()
+      publicacion.sumarUsuario(this)
+    }
+  }
+  //fun desLikear(publicacion: Publicacion){ publicacion.sacarLike() } //Definir sacarLike() y sacar usuario.
+  //fun cambiarCalidadVideo(nuevaCalidad){}
 
+  fun publicacionesFotos() = publicaciones.filter{ it.tipoPublicacion() == "foto" }
+  fun modificarFactorCompresionFotos(nuevoFactor : Double){
+    this.publicacionesFotos().forEach{it.cambiarFactorDeCompresion(nuevoFactor)}
+  } //Consultar
+  fun agregarAmigo(nuevoUsuario: Usuario) {
+    if(!amigos.contains(nuevoUsuario))
+      amigos.add(nuevoUsuario)
+      nuevoUsuario.amigos.add(this)
+  }
+  fun eliminarAmigo(nuevoUsuario: Usuario) {
+    if (amigos.contains(nuevoUsuario))
+    amigos.remove(nuevoUsuario)
+    nuevoUsuario.amigos.remove(this)
+  }
 
+  fun puedoVerYo(publicacion: Publicacion) = publicaciones.contains(publicacion)
 
 }
