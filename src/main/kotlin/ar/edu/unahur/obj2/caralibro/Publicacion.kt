094c0afa1cc49eee09681612fed0c769a07fb4d7
Luch <70541571+lucianoaraus@users.noreply.github.com>
2021-04-18 22:04:32 -0300

    Update - July Lucho Publicacion

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
@@ -3,44 +3,51 @@ package ar.edu.unahur.obj2.caralibro
 import kotlin.math.ceil
 
 abstract class Publicacion(var likes: Int = 0, var permiso: String = "Publico") { //Agregue el atributo **likes**
-  val likers = mutableListOf<Usuario>()
+  val likers = mutableListOf<Usuario>() //Ver
   abstract fun espacioQueOcupa(): Int
-
   fun cantLikes() = likes
   fun sumarLike(){ likes += 1 }
+  fun sumarUsuario(usuario: Usuario) {
+    if (!likers.contains(usuario)){
+      likers.add(usuario)
+    }
+  }
   fun quienLikeo() = likers
-  fun cambiarPermiso(nuevoPermiso : String){ permiso = nuevoPermiso}
+  abstract fun tipoPublicacion() : String
+  fun cambiarPermiso(nuevoPermiso : String){ permiso = nuevoPermiso }
   fun quienPuedeVer(){} //Que usuario puede ver esta publicacion(publico, solo amigos, privado con lista, publico con lista)
+
+
 }
 
-class Foto(val alto: Int, val ancho: Int) : Publicacion() {
-  val factorDeCompresion = 0.7
+class Foto(val alto: Int, val ancho: Int, var factorDeCompresion: Double = 0.7) : Publicacion() {
   override fun espacioQueOcupa() = ceil(alto * ancho * factorDeCompresion).toInt() //ceil() => Redondea un numero desde positiva hasta infinito.
+  override fun tipoPublicacion() = "foto"
+  fun cambiarFactorDeCompresion(nuevoFactor : Double) { factorDeCompresion = nuevoFactor }
 }
 
 class Texto(val contenido: String) : Publicacion() {
   override fun espacioQueOcupa() = contenido.length
+  override fun tipoPublicacion() = "texto"
 }
 
 class Video(var calidad: String, val duracion: Int) : Publicacion() {
   override fun espacioQueOcupa(): Int {
-    val espacio = 0
+    var espacio = 0
     if(calidad == "SD"){
-      espacio = this.duracionSeg()
+      espacio = duracion
     }
     else if(calidad == "HD 720p"){
-      espacio = this.duracionSeg() * 3
+      espacio = duracion * 3
     }
-    else {
-      espacio = this.duracionSeg() * 6
+    else if (calidad == "Full HD 1080p"){
+      espacio = duracion * 6
     }
     return espacio
   }
-  fun cambiarCalidad(nuevaCalidad : String){
-    calidad = nuevaCalidad
-  }
-  fun duracionSeg(){} //Convierte la duracion total del video en segundos.
-
+  fun cambiarCalidad(nuevaCalidad : String){ calidad = nuevaCalidad }
+  fun duracionSeg() = duracion
+  override fun tipoPublicacion() = "video"
 }
 
 
