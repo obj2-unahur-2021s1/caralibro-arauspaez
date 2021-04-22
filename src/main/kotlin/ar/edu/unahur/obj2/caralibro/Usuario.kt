21320617ab77686dbfcba3525607c56c41db5272
lucianoaraus <lucianoaraus97@gmail.com>
2021-04-22 17:13:58 -0300

    Update: hacer mejoresAmigos (no estaba terminado)

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Usuario.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Usuario.kt
@@ -18,5 +18,18 @@ class Usuario {
       amigos.add(nuevoUsuario)
       nuevoUsuario.amigos.add(this)
   }
-  fun puedoVerYo() = publicaciones.size > 0 //Mientras haya publicaciones puede ver
+  fun puedoVerYo() = publicaciones.size > 0 //Un usuario puede ver sus propias publicaciones
+  fun puedoVerPublicacion(publicacion: Publicacion, usuario: Usuario) =
+    publicacion.puedeVerPublicacion(usuarioChusma = this, usuarioDueño = usuario) //Saber si un usuario puede ver cierta publicacion.
+
+  //fun mejoresAmigos() = retorna una lista con los usuarios que pueden ver todas sus publicaciones**
+
+  fun cantidadAmigos() = amigos.size
+  fun esMasAmistosoQue(usuario: Usuario) = this.cantidadAmigos() > usuario.cantidadAmigos()
+  fun amigoMasPopular() = amigos.maxBy { it.totalLikes() } //Devuelve el amigo con mas likes entre todas sus publicaciones.
+  fun totalLikes() = publicaciones.sumBy { it.cantidadLikes() } //Devuelve el total de likes de la lista de publicaciones.
+  fun totalPublicaciones() = publicaciones.size
+  fun esStalker(stalker: Usuario) =
+    this.cantPublicacionesLikeadasPorAmigo(stalker) > this.totalPublicaciones() * 0.9 //usuario es stalker si likeo mas del 90% de mis publicaciones.
+  fun cantPublicacionesLikeadasPorAmigo(amigo: Usuario) = publicaciones.count { it.likers.contains(amigo) } //Devuelve el total de publicaciones likeadas por 'amigo'.
 }
