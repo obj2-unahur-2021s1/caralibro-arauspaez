e9440313dcb30663068efb34bbce45c5bd9d4cce
JulianPaez92 <70548446+JulianPaez92@users.noreply.github.com>
2021-04-24 21:29:19 -0300

    Update Usuario.kt

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Usuario.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Usuario.kt
@@ -8,9 +8,9 @@ class Usuario {
     publicaciones.add(publicacion)
   }
   fun espacioDePublicaciones() = publicaciones.sumBy { it.espacioQueOcupa() }
-  fun likear(publicacion: Publicacion) {
-    if (!publicacion.quienLikeo().contains(this)){
-      publicacion.sumarUsuarioConLike(this)
+  fun meLikea(publicacion: Publicacion,usuario: Usuario) {
+    if (!publicacion.quienLikeo().contains(usuario)){
+      publicacion.sumarUsuarioConLike(usuario)
     }
   }
   fun agregarAmigo(nuevoUsuario: Usuario) {
@@ -23,17 +23,21 @@ class Usuario {
       excluidos.add(nuevoUsuario)
   }
   fun puedoVerYo() = publicaciones.size > 0 //Un usuario puede ver sus propias publicaciones
-  fun puedoVerPublicacion(publicacion: Publicacion, usuario: Usuario) =
-    publicacion.puedeVerPublicacion(usuarioChusma = this, usuarioDueño = usuario) //Saber si un usuario puede ver cierta publicacion.
+  /*fun puedoVerPublicacion(usuarioChusma : Usuario,publicacion: Publicacion) = //Saber si un usuario puede ver cierta publicacion.
+    publicacion.puedeVerPublicacion(usuarioChusma,this)*/
 
   //fun mejoresAmigos() = retorna una lista con los usuarios que pueden ver todas sus publicaciones**
+  /*fun mejoresAmigos(usuario: Usuario){
+    publicaciones.forEach{ it.puedeVerPublicacion(usuarioChusma = usuario,usuarioDuenio = this) }
+  }*/
 
   fun cantidadAmigos() = amigos.size
   fun esMasAmistosoQue(usuario: Usuario) = this.cantidadAmigos() > usuario.cantidadAmigos()
-  fun amigoMasPopular() = amigos.maxBy { it.totalLikes() } //Devuelve el amigo con mas likes entre todas sus publicaciones.
+  fun amigoMasPopular() = amigos.maxByOrNull { it.totalLikes() } //Devuelve el amigo con mas likes entre todas sus publicaciones.
   fun totalLikes() = publicaciones.sumBy { it.cantidadLikes() } //Devuelve el total de likes de la lista de publicaciones.
   fun totalPublicaciones() = publicaciones.size
   fun esStalker(stalker: Usuario) =
     this.cantPublicacionesLikeadasPorAmigo(stalker) > this.totalPublicaciones() * 0.9 //usuario es stalker si likeo mas del 90% de mis publicaciones.
   fun cantPublicacionesLikeadasPorAmigo(amigo: Usuario) = publicaciones.count { it.likers.contains(amigo) } //Devuelve el total de publicaciones likeadas por 'amigo'.
+
 }
